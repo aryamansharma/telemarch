45b695914cfaf17fcfcb5e7a8a3cbeda85924a40
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2023-10-02 14:58:39 +0530

    setting up firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  firestore: Firestore = inject(Firestore);
   title = 'telemarch';
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'telemarch-app',
+    appId: '1:214547554471:web:9769913cfc2cf0f008fb82',
+    storageBucket: 'telemarch-app.appspot.com',
+    apiKey: 'AIzaSyDPnHCwDwquFIjV5HpdV10YiOY7IWmGBXI',
+    authDomain: 'telemarch-app.firebaseapp.com',
+    messagingSenderId: '214547554471',
+    measurementId: 'G-YK0V9WJFPY',
+  },
+};
