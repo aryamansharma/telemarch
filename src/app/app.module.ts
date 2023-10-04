ce278d9795a5ee28bb166bfd4994dd30c55fb7fe
aryamansharma <49034013+aryamansharma@users.noreply.github.com>
2023-10-05 01:49:27 +0530

    adding responsive navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { PostsComponent } from './account-components/home/posts/posts.component';
+import { HomeComponent } from './account-components/home/home.component';
+import { AccountSettingsComponent } from './account-components/account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    PostsComponent,
+    HomeComponent,
+    AccountSettingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,9 +33,9 @@ import { MainComponent } from './main/main.component';
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isNavbarOpened: boolean = false;
+  @ViewChild('navbar') navbar!: ElementRef;
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  openNavbar(event: any) {
+    if (this.navbar.nativeElement.getAttribute('data-visible') === 'false') {
+      this.isNavbarOpened = true;
+      this.navbar.nativeElement.setAttribute('data-visible', 'true');
+    } else if (
+      this.navbar.nativeElement.getAttribute('data-visible') === 'true'
+    ) {
+      this.isNavbarOpened = false;
+      this.navbar.nativeElement.setAttribute('data-visible', 'false');
+    }
+  }
 }
